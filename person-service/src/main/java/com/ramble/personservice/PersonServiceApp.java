f51a360cc9942a062441aa4d36af2ccba7e435f0
Cml <cnaylor@163.com>
2022-10-25 16:43:04 +0800

    init

---
+++ person-service/src/main/java/com/ramble/personservice/PersonServiceApp.java
@@ -0,0 +1,13 @@
+package com.ramble.personservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonServiceApp.class, args);
+    }
+
+}
