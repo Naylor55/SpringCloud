35bf4a63c94b638f059952ae595bb93429c903bd
Cml <cnaylor@163.com>
2022-10-26 09:44:45 +0800

    init

---
+++ ramble-gateway/src/main/java/com/ramble/ramblegateway/RambleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ramble.ramblegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RambleGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RambleGatewayApplication.class, args);
+    }
+
+}
